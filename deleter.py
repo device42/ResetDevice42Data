444bd29640f27bd9a58ea65e87822053b6a77ed0
Raj Jalan <rjalan2@gmail.com>
2015-11-06 10:42:19 -0500

    add files

---
+++ deleter.py
@@ -0,0 +1,134 @@
+import sys
+import requests
+import base64
+
+try:
+    requests.packages.urllib3.disable_warnings()
+except:
+    pass
+
+D42_USER    = 'admin'
+D42_PWD     = 'adm!nd42'
+D42_URL     = 'https://192.168.3.30'
+
+
+class Wipe():
+    def __init__(self):
+        self.headers = {
+            'Authorization': 'Basic ' + base64.b64encode(D42_USER + ':' + D42_PWD),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+    def delete_racks(self):
+        """
+        Deletes racks
+        :return:
+        """
+        print '\n[!] Deleting racks'
+        f = '/api/1.0/racks/'
+        url = D42_URL+f
+        response = requests.get(url,headers=self.headers, verify=False)
+        raw = response.json()
+        racks = [x['rack_id'] for x in raw['racks']]
+
+        for rack in racks:
+            print '\t[-] Rack ID: %s' % rack
+            f = '/api/1.0/racks/%s/' % rack
+            url = D42_URL + f
+            r = requests.delete(url, headers=self.headers, verify=False)
+
+    def delete_buildings(self):
+        """
+        Deletes buildings and rooms as well
+        :return:
+        """
+        print '\n[!] Deleting buildings'
+        f = '/api/1.0/buildings/'
+        url = D42_URL+f
+        response = requests.get(url,headers=self.headers, verify=False)
+        raw = response.json()
+
+        buildings = [x['building_id'] for x in raw['buildings']]
+
+        for building in buildings:
+            print '\t[-] Building ID: %s' % building
+            f = '/api/1.0/buildings/%s/' % building
+            url = D42_URL + f
+            r = requests.delete(url, headers=self.headers, verify=False)
+
+    def delete_pdus(self):
+        """
+        Deletes PDUs, but it does not delete PDU models
+        :return:
+        """
+        print '\n[!] Deleting pdus'
+        f = '/api/1.0/pdus/'
+        url = D42_URL+f
+        response = requests.get(url,headers=self.headers, verify=False)
+        raw = response.json()
+
+        pdus = [x['pdu_id'] for x in raw['pdus']]
+
+        for pdu in pdus:
+            print '\t[-] PDU ID: %s' % pdu
+            f = '/api/1.0/pdus/%s/' % pdu
+            url = D42_URL + f
+            r = requests.delete(url, headers=self.headers, verify=False)
+
+
+    def delete_subnets(self):
+        """
+        Deletes subnets and IPs as well
+        :return:
+        """
+        print '\n[!] Deleting subnets'
+        f = '/api/1.0/subnets/'
+        url = D42_URL+f
+        response = requests.get(url,headers=self.headers, verify=False)
+        raw = response.json()
+
+        subnets = [x['subnet_id'] for x in raw['subnets']]
+
+        for subnet in subnets:
+            print '\t[-] Subnet ID: %s' % subnet
+            f = '/api/1.0/subnets/%s/' % subnet
+            url = D42_URL + f
+            r = requests.delete(url, headers=self.headers, verify=False)
+
+    def delete_devices(self):
+        """
+        Deleting device one at the time. Very slow!
+        :return:
+        """
+        print '\n[!] Deleting devices'
+        f = '/api/1.0/devices/'
+        url = D42_URL+f
+        response = requests.get(url,headers=self.headers, verify=False)
+        raw = response.json()
+
+        devices = [x['device_id'] for x in raw['Devices']]
+        total = len(devices)
+        i = 1
+        for device in devices:
+            print '\t[-] Device ID: %s [%d of %d]' % (device, i, total)
+            f = '/api/1.0/devices/%s/' % device
+            url = D42_URL + f
+            r = requests.delete(url, headers=self.headers, verify=False)
+            i+=1
+
+
+
+def main():
+    w = Wipe()
+    w.delete_racks()
+    w.delete_buildings()
+    w.delete_pdus()
+    w.delete_subnets()
+    w.delete_devices()
+
+
+
+if __name__ == '__main__':
+    main()
+    print '\n[!] Done!'
+    sys.exit()
